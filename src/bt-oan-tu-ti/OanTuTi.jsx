f3d0e08bfb1d71213305070ada4ebcde5ad35127
ToanNguyen99 <43871385+ToanNguyen99@users.noreply.github.com>
2021-08-09 20:11:02 +0700

    BT Oan Tu Ti

---
+++ src/bt-oan-tu-ti/OanTuTi.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class OanTuTi extends Component {
+    render() {
+        return (
+            <div>
+                OanTuTi
+            </div>
+        )
+    }
+}
